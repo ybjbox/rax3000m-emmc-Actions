97207889bfb9fedd5f603c4651a72d7ee28863f9
371077385 <103187857+371077385@users.noreply.github.com>
2023-10-10 23:54:18 +0800

    Update diy-part2.sh

--- hanwckf/CMCC-RAX3000M-EMMC/diy-part2.sh
+++ hanwckf/CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
